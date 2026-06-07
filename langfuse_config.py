0ada9eb4dd48f8057692d553c460321cd7b02425
Kona Sowmya <ksowmya1807@gmail.com>
2026-06-07 14:16:23 +0530

    Integrate Langfuse for tracing and monitoring

---
+++ langfuse_config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+from langfuse import Langfuse
+
+load_dotenv()
+
+langfuse = Langfuse(
+    secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
+    public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
+    host=os.getenv("LANGFUSE_HOST")
+)
+
+print("Langfuse Initialized Successfully")
