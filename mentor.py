e6884a5d0c4d863c1f28cc9a3beff628934adbf9
Kona Sowmya <ksowmya1807@gmail.com>
2026-06-07 14:03:28 +0530

    Added AI mentor for Learning doubts assistance

--- mentor.py
+++ mentor.py
@@ -1,27 +1,30 @@
 from dotenv import load_dotenv
-
 load_dotenv()
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     temperature=0.7
 )
 
-
 def ask_mentor(question):
+
+    print("QUESTION RECEIVED:", question)
 
     prompt = f"""
     You are an AI Learning Mentor.
 
-    Answer the user's learning question clearly and simply.
+    Answer clearly and simply.
 
     Question:
     {question}
     """
 
+    print("QUESTION:", question)
+
     response = llm.invoke(prompt)
+
+    print("RESPONSE:", response.content)
 
     return response.content
