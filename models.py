14a0d4e3ee42acd67e21af1fde56b206c3294a7b
Kona Sowmya <ksowmya1807@gmail.com>
2026-06-07 14:04:39 +0530

    Added Pydantic Input validation models

--- models.py
+++ models.py
@@ -1,20 +1,9 @@
 from pydantic import BaseModel, Field
 
-
 class UserInput(BaseModel):
 
-    skill: str = Field(
-        min_length=2,
-        max_length=100
-    )
-
+    skill: str = Field(min_length=2, max_length=100)
     level: str
-
     goal: str
-
-    hours: int = Field(
-        ge=1,
-        le=10
-    )
-
+    hours: int = Field(ge=1,le=10)
     style: str
