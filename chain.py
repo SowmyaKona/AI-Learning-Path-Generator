c0070a22053d1f70b0fa73aed5822a15ffe10ba0
Kona Sowmya <ksowmya1807@gmail.com>
2026-06-10 08:36:16 +0530

    Build a langchain pipeline for Roadmap Generation

--- chain.py
+++ chain.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-# import langfuse_config
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langfuse  import observe
-
+from langfuse.langchain import CallbackHandler
 from prompt import prompt
 from parser import parser
+
+#-------- Langfuse Callback Handler------
+langfuse_handler = CallbackHandler()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite",temperature=0.7)
 
@@ -20,13 +22,13 @@ chain = (prompt.partial(format_instructions=parser.get_format_instructions())| l
 @observe()
 def generate_learning_path(user_input):
     response = chain.invoke({
-        "chat_history": "",
         "skill": user_input.skill,
         "level": user_input.level,
         "goal": user_input.goal,
-        "hours": user_input.hours,
-        "style": user_input.style
-    })
-
+        "style": user_input.style},
+    config={
+            "callbacks": [langfuse_handler]
+        }
+    )
     parsed_response = parser.parse(response.content)
     return parsed_response
